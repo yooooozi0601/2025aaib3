a26c3620ecb3112d43da13c1b4a70179fe80979e
yooooozi0601 <0933138398yoyo@gmail.com>
2025-09-17 11:32:42 +0800

    week02

---
+++ week02-2b.cpp
@@ -0,0 +1,16 @@
+//week02-2b
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string a, ans;
+    cin >> a;
+
+    for(int i=a.length()-1; i>=0; i--){
+        ans += a[i];
+    }
+    cout << a << '+' << stoi(ans) << '=';
+    cout << stoi(a) + stoi(ans) << endl;
+
+}
