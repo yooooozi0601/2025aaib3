4727b412fe060fbc31dd336c8c8448b646e44ba2
yooooozi0601 <0933138398yoyo@gmail.com>
2025-09-17 11:34:41 +0800

    week02

---
+++ week02/week02-3.cpp
@@ -0,0 +1,16 @@
+///week02-3.cpp
+///1768
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+            int N1=word1.length();
+            int N2=word2.length();
+            string ans;
+            for(int i=0;i<max(N1,N2) ;i++){
+                if(i<N1) ans += word1[i];
+                if(i<N2) ans += word2[i];
+            }
+            return ans;
+
+    }
+};
