b85ca9ee3e05919d4b1d4e4a6b2b61352a881bd0
yooooozi0601 <0933138398yoyo@gmail.com>
2025-11-19 11:44:05 +0800

    week11

---
+++ week11/week11-1b.cpp
@@ -0,0 +1,12 @@
+///week11-1b.cpp
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        set<int> numsSet;
+        for(int num : nums) numsSet.insert(num);
+        while( numsSet.find(original) !=numsSet.end()){
+            original =original *2;
+        }
+        return original;
+    }
+};
